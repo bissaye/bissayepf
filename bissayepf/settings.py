7017609a290e8e3336e87de99c215931c5d70318
bissaye <bissaye.com@gmail.com>
2020-10-28 21:43:44 +0100

    ajout du localhost a la varable ALLOPWED HOST

--- bissayepf/settings.py
+++ bissayepf/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['bissayepf.herokuapp.com']
+ALLOWED_HOSTS = ['bissayepf.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -135,9 +135,9 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATICFILES_DIRS =(
+    STATICFILES_DIRS =[
         os.path.join(PROJECT_ROOT, 'static'),
-    )
+    ]
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     db_from_env  = dj_database_url.config(conn_max_age=500)
