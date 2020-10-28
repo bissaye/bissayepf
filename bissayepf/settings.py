98d61319ad95ebfa0630ee08e5d910a2aafd476c
bissaye <bissaye.com@gmail.com>
2020-10-28 21:26:39 +0100

    modificationn des settings

--- bissayepf/settings.py
+++ bissayepf/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','bissayepf.herokuapp.com']
+ALLOWED_HOSTS = ['bissayepf.herokuapp.com']
 
 
 # Application definition
